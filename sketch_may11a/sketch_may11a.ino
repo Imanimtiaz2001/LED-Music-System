20fcb5d71cd8675304ab1b44ba6c0ba8b1a8e68c
Iman imtiaz <75875787+Imanimtiaz2001@users.noreply.github.com>
2022-05-11 23:57:40 +0500

    Add files via upload

---
+++ sketch_may11a/sketch_may11a.ino
@@ -0,0 +1,17 @@
+//led on esp32
+int LED=2;
+void setup() {
+  // put your setup code here, to run once:
+//set the led on esp32 as output
+pinMode(LED ,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("LED is on.");
+digitalWrite(LED ,HIGH);//turn the led on (voltage high)
+delay(2000);//keep the led on for 2 secs
+Serial.print("LED is off.");
+digitalWrite(LED ,LOW);//turn the led off (voltage low)
+delay(1000);//keep the led off for 1 sec
+}
